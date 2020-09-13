9119b2ba5ec228ef90c49f7af0d91a4b70add230
amitaysoffer <amitaysof@gmail.com>
2020-09-13 16:55:01 +0100

    Add and subtract are working completely

--- script.js
+++ script.js
@@ -18,122 +18,139 @@ document.querySelectorAll('.number').forEach(item => {
   item.addEventListener('click', function (e) {
 
     let addNumber = e.target.getAttribute('data-value');
-
+    // Checking first number
     if (checkSecondNumber) {
       if (firstNumber === undefined) {
         firstNumber = addNumber
-        console.log(`First Number ${firstNumber}`);
-        // append
+        // console.log(`First Number ${firstNumber}`);
+
         process.innerText = firstNumber
-        console.log(typeof process.innerText)
+        // console.log(typeof process.innerText)
       } else {
         firstNumber += addNumber
-        console.log(`First Number ${firstNumber}`);
-        //  append
+        // console.log(`First Number ${firstNumber}`);
+
         process.innerText = firstNumber
       }
+      // Checking second number
     } else {
-      debugger;
+      // debugger;
+      // Checking first digit
       if (secondNumber === undefined) {
         secondNumber = addNumber
-        console.log(`Second Number ${secondNumber}`);
-        if (tempAnswer) {
-          process.innerText = tempAnswer + parseInt(secondNumber)
-        } else {
-          process.innerText += secondNumber
-        }
+        // console.log(`Second Number ${secondNumber}`);
+        process.innerText += secondNumber
+        // if (!tempAnswer) {
+        //   process.innerText = tempAnswer + parseInt(secondNumber)
+        // } else {
+        // }
         // tempAnswer = secondNumber
       } else {
+        // checking second digit
         // debugger
         secondNumber += addNumber
-        console.log(`Second Number ${secondNumber}`);
-        tempAnswer = secondNumber;
-        answer.innerText += addNumber
+        // console.log(`Second Number ${secondNumber}`);
+        // tempAnswer = secondNumber;
+        process.innerText += addNumber
       }
     }
   });
 });
 
-
+// ADD NUMBER EVENT
 plus.addEventListener('click', function () {
-  console.log('PLUS')
   // debugger;
-  if (!firstNumber) {
-    alert('click number first');
-  } else if (!secondNumber) {
+  if (!secondNumber) {
     // debugger;
     process.innerText += '+'
     operator = '+'
     return checkSecondNumber = false;
   } else {
-    debugger;
+    // debugger;
     process.innerText += '+'
 
-    tempAnswer = parseInt(firstNumber) + parseInt(secondNumber)
+    if (firstNumber) {
+      if (operator === '+') {
+        tempAnswer = parseInt(firstNumber) + parseInt(secondNumber)
+      } else if (operator === '-') {
+        tempAnswer = parseInt(firstNumber) - parseInt(secondNumber)
+      }
+    } else {
+      if (operator === '+') {
+        tempAnswer = parseInt(tempAnswer) + parseInt(secondNumber)
+      } else if (operator === '-') {
+        tempAnswer = parseInt(tempAnswer) - parseInt(secondNumber)
+      }
+    }
+
 
     answer.innerText = tempAnswer
-    // let a = parseInt(firstNumber)
-    // let b = parseInt(secondNumber)
+
+    operator = '+'
     secondNumber = undefined
-    // firstNumber = Number(firstNumber) + Number(secondNumber);
-    // return firstNumber = a + b;
+    firstNumber = undefined
   }
 });
 
-// 
+// SUBTRACT NUMBER EVENT
+minus.addEventListener('click', function () {
+  // debugger;
+  if (!secondNumber) {
+    // debugger;
+    process.innerText += '-'
+    operator = '-'
+    return checkSecondNumber = false;
+  } else {
+    // debugger;
+    process.innerText += '-'
 
-// Final answer of equasion
+    if (firstNumber) {
+      if (operator === '+') {
+        tempAnswer = parseInt(firstNumber) + parseInt(secondNumber)
+      } else if (operator === '-') {
+        tempAnswer = parseInt(firstNumber) - parseInt(secondNumber)
+      }
+    } else {
+      if (operator === '+') {
+        tempAnswer = tempAnswer + parseInt(secondNumber)
+      } else if (operator === '-') {
+        tempAnswer = tempAnswer - parseInt(secondNumber)
+      }
+    }
+
+
+    answer.innerText = tempAnswer
+
+    operator = '-'
+    secondNumber = undefined
+    firstNumber = undefined
+  }
+});
+
+
+// ANSWER of equasion
 document.getElementById('equal').addEventListener('click', function (e) {
 
-  debugger;
-  console.log(process.innerText);
+  // debugger;
 
-  console.log(firstNumber)
-  console.log(secondNumber)
-  console.log(tempAnswer)
-  console.log(operator)
 
   let newNum1 = parseInt(firstNumber)
   let newNum2 = parseInt(secondNumber)
-  let newTempAnswer = parseInt(tempAnswer)
+  // tempAnswer = parseInt(tempAnswer)
 
-  // if (!tempAnswer) {
-  if (operator === '+') {
-    answer.innerText = newNum1 + newNum2
+  if (!tempAnswer) {
+    if (operator === '+') {
+      answer.innerText = newNum1 + newNum2
+    } else if (operator === '-') {
+      answer.innerText = newNum1 - newNum2
+    }
+  } else {
+    if (operator === '+') {
+      answer.innerText = tempAnswer + newNum2
+    } else if (operator === '-') {
+      answer.innerText = tempAnswer - newNum2
+    }
   }
-  // } else {
-  //   if (operator === '+') {
-  //     answer.innerText = newTempAnswer + newNum2
-  //   } else if (operator === '-') {
-  //     answer.innerText = newTempAnswer - newNum2
-  //   }
-  // }
 })
 
 
-// ------------------------------- 
-minus.addEventListener('click', function () {
-  alert('not available at the moment')
-  //   console.log('MINUS')
-  //   // debugger;
-  //   if (!firstNumber) {
-  //     alert('click number first');
-  //   } else if (!secondNumber) {
-  //     // debugger;
-  //     process.innerText += '-'
-  //     operator = '-'
-  //     return checkSecondNumber = false;
-  //   } else {
-  //     // debugger;
-  //     process.innerText += '-'
-  
-  //     tempAnswer = parseInt(firstNumber) - parseInt(secondNumber)
-  
-  //     answer.innerText = tempAnswer
-  //     // let a = parseInt(firstNumber)
-  //     // let b = parseInt(secondNumber)
-  //     secondNumber = undefined
-  //     // firstNumber = Number(firstNumber) + Number(secondNumber);
-  //     // return firstNumber = a + b;
-  //   }
-  });
