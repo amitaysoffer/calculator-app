d1484d9c30188e3395a24c59fc8cb04636da7277
amitaysoffer <amitaysof@gmail.com>
2020-09-30 20:13:43 +0100

    reorganized code

--- script.js
+++ script.js
@@ -9,250 +9,152 @@ const decimal = document.getElementById('decimal');
 const equal = document.getElementById('equal');
 const clear = document.getElementById('clear-btn');
 
-let initialNumber;
-let lastNumber;
+let firstOperhand;
+let secondOperhand;
 let operator = ''
-let currentAnswer = 0;
-let isInitialNumberOneDigitOrLess = true;
 let isDecimal = false;
 
-class UI {
-  static styleOperatorButtons(selectedOperator) {
-    const operatorList = ['add', 'minus', 'multiply', 'divide'];
-    const result = operatorList.filter(operator => operator != selectedOperator.id)
-    // Style the clicked operator
-    selectedOperator.classList.add('operator-clicked')
-    // Style rest operators
-    document.getElementById(result[0]).classList.remove('operator-clicked');
-    document.getElementById(result[1]).classList.remove('operator-clicked');
-    document.getElementById(result[2]).classList.remove('operator-clicked');
+function setFirstOperhand(number) {
+  if (!firstOperhand) {
+    firstOperhand = number
+    process.innerText = firstOperhand
+  } else {
+    firstOperhand += number
+    process.innerText = firstOperhand
   }
-  static errorMessage(operator) {
-    alert(`You must enter a number before you enter '${operator}'`);
+}
+
+function setSecondOperhand(number) {
+  if (!secondOperhand) {
+    secondOperhand = number
+    process.innerText += secondOperhand
+  } else {
+    secondOperhand += number
+    process.innerText += number
   }
-  static renderMinimizedRemainder() {
-    const text = answer.innerText;
-    answer.innerText = text.substring(0, text.indexOf('.') + 3);
+}
+
+function styleOperatorButtons(operatorElement) {
+  const operatorList = ['add', 'minus', 'multiply', 'divide'];
+  const result = operatorList.filter(operator => operator != operatorElement.id)
+  // Style the clicked operator
+  operatorElement.classList.add('operator-clicked')
+  // Style rest of operators
+  document.getElementById(result[0]).classList.remove('operator-clicked');
+  document.getElementById(result[1]).classList.remove('operator-clicked');
+  document.getElementById(result[2]).classList.remove('operator-clicked');
+}
+
+function errorMessage(operator) {
+  alert(`You must enter a number before you enter '${operator}'`);
+}
+
+function renderMinimizedRemainder() {
+  const text = answer.innerText;
+  answer.innerText = text.substring(0, text.indexOf('.') + 3);
+}
+
+function performCalculation() {
+  switch (operator) {
+    case '+':
+      return Number(firstOperhand) + Number(secondOperhand);
+    case '-':
+      return Number(firstOperhand) - Number(secondOperhand);
+    case 'x':
+      return Number(firstOperhand) * Number(secondOperhand);
+    case '÷':
+      return Number(firstOperhand) / Number(secondOperhand);
   }
-  static renderAnswer(selectedNumber, lastOperator) {
-    switch (lastOperator) {
-      case '+':
-        answer.innerText = Number(selectedNumber) + Number(lastNumber);
-        break;
-      case '-':
-        answer.innerText = Number(selectedNumber) - Number(lastNumber);
-        break;
-      case 'x':
-        answer.innerText = Number(selectedNumber) * Number(lastNumber);
-        break;
-      case '÷':
-        answer.innerText = Number(selectedNumber) / Number(lastNumber);
+}
+
+function restartOperatorsBtnStyle() {
+  add.classList.remove('operator-clicked')
+  minus.classList.remove('operator-clicked')
+  multiply.classList.remove('operator-clicked')
+  divide.classList.remove('operator-clicked')
+}
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    const currentOperator = e.target.textContent;
+    // Check if operator is pressed before number
+    if (!operator && !firstOperhand && !secondOperhand) {
+      return errorMessage(currentOperator);
     }
-  }
-  static restartOperatorsBtnStyle() {
-    add.classList.remove('operator-clicked')
-    minus.classList.remove('operator-clicked')
-    multiply.classList.remove('operator-clicked')
-    divide.classList.remove('operator-clicked')
-  }
-}
+    // Fix if user presses operator when operator is in play
+    if (operator && !secondOperhand) {
+      process.innerText = process.innerText.slice(0, process.innerText.length - 1);
+    }
+    // check to see if an operation is ongoing
+    if (operator && secondOperhand) {
+      const result = performCalculation()
+      answer.innerText = result;
+      firstOperhand = result;
+      secondOperhand = undefined;
+    }
 
-class Utilities {
-  static switchBetweenOperatorsFix(newOperator) {
-    process.innerText = process.innerText.slice(0, process.innerText.length - 1) + newOperator
-    return operator = newOperator
-  }
-  static addConditionsToInitialNumber(newOperator) {
-    process.innerText += newOperator
-    isInitialNumberOneDigitOrLess = false;
-    operator = newOperator
-  }
-  static updateSettings(newOperator) {
-    operator = newOperator;
-    currentAnswer = answer.innerText
-    process.innerText += newOperator
-    lastNumber = undefined
-    initialNumber = undefined
-  }
-}
-
-// Event Handlers
-add.addEventListener('click', function () {
-  // Adjust display operator if user changes operators
-  if (operator && !lastNumber) {
-    Utilities.switchBetweenOperatorsFix('+');
-    // User clicks an operator before number
-  } else if (!operator && !initialNumber && !lastNumber) {
-    return UI.errorMessage('+');
-  } else if (!lastNumber) {
-    Utilities.addConditionsToInitialNumber('+');
-  }
-
-  // Create current answer of initial number
-  if (initialNumber && lastNumber) {
-    UI.renderAnswer(initialNumber, operator, '+');
-    Utilities.updateSettings('+');
-    // Update current answer
-  } else if (lastNumber) {
-    UI.renderAnswer(currentAnswer, operator, '+');
-    Utilities.updateSettings('+');
-  }
-
-  if (isDecimal) {
-    UI.renderMinimizedRemainder();
-  };
-
-  UI.styleOperatorButtons(this);
-});
-
-minus.addEventListener('click', function () {
-  // Adjust display operator if user changes operators
-  if (operator && !lastNumber) {
-    Utilities.switchBetweenOperatorsFix('-');
-    // User clicks an operator before number
-  } else if (!operator && !initialNumber && !lastNumber) {
-    return UI.errorMessage('-');
-  } else if (!lastNumber) {
-    Utilities.addConditionsToInitialNumber('-');
-  }
-  // Create current answer of initial number
-  if (initialNumber && lastNumber) {
-    UI.renderAnswer(initialNumber, operator);
-    Utilities.updateSettings('-');
-    // Update current answer
-  } else if (lastNumber) {
-    UI.renderAnswer(currentAnswer, operator);
-    Utilities.updateSettings('-');
-  }
-
-  if (isDecimal) {
-    UI.renderMinimizedRemainder();
-  };
-
-  UI.styleOperatorButtons(this);
-});
-
-multiply.addEventListener('click', function () {
-  // Adjust display operator if user changes operators
-  if (operator && !lastNumber) {
-    Utilities.switchBetweenOperatorsFix('x');
-    // User clicks an operator before number
-  } else if (!operator && !initialNumber && !lastNumber) {
-    return UI.errorMessage('x');
-  } else if (!lastNumber) {
-    Utilities.addConditionsToInitialNumber('x');
-  }
-  // Create current answer of initial number
-  if (initialNumber && lastNumber) {
-    UI.renderAnswer(initialNumber, operator);
-    Utilities.updateSettings('x');
-    // Update current answer
-  } else if (lastNumber) {
-    UI.renderAnswer(currentAnswer, operator);
-    Utilities.updateSettings('x');
-  }
-
-  if (isDecimal) {
-    UI.renderMinimizedRemainder();
-  };
-
-  UI.styleOperatorButtons(this);
-});
-
-divide.addEventListener('click', function () {
-  // Adjust display operator if user changes operators
-  if (operator && !lastNumber) {
-    Utilities.switchBetweenOperatorsFix('÷');
-    // User clicks an operator before number
-  } else if (!operator && !initialNumber && !lastNumber) {
-    return UI.errorMessage('÷');
-  } else if (!lastNumber) {
-    Utilities.addConditionsToInitialNumber('÷');
-  }
-  // Create current answer of initial number
-  if (initialNumber && lastNumber) {
-    UI.renderAnswer(initialNumber, operator);
-    Utilities.updateSettings('÷');
-    // Update current answer
-  } else if (lastNumber) {
-    UI.renderAnswer(currentAnswer, operator);
-    Utilities.updateSettings('÷');
-  }
-
-  if (isDecimal) {
-    UI.renderMinimizedRemainder();
-  };
-
-  UI.styleOperatorButtons(this);
-});
+    operator = currentOperator;
+    process.innerText += operator;
+    styleOperatorButtons(e.target);
+  });
+})
 
 decimal.addEventListener('click', function () {
   const text = process.innerText;
   if (text.charAt(text.length - 1) !== '.' && text) {
-    if (initialNumber && !lastNumber) {
-      initialNumber += '.'
+    if (firstOperhand && !secondOperhand) {
+      firstOperhand += '.'
       process.innerText += '.'
     } else {
-      lastNumber += '.'
+      secondOperhand += '.'
       process.innerText += '.'
     }
   } else {
-    UI.errorMessage('decimal')
+    errorMessage('decimal')
   }
   isDecimal = true;
 });
 
 clear.addEventListener('click', function (e) {
-  initialNumber = undefined;
-  lastNumber = undefined;
+  firstOperhand = undefined;
+  secondOperhand = undefined;
   operator = ''
-  currentAnswer = 0;
-  isInitialNumberOneDigitOrLess = true;
   isDecimal = false;
 
   process.innerText = '';
   answer.innerText = '0';
 
-  UI.restartOperatorsBtnStyle();
+  restartOperatorsBtnStyle();
 })
 
 equal.addEventListener('click', function (e) {
-  if (initialNumber) {
-    UI.renderAnswer(initialNumber, operator);
-  } else if (lastNumber) {
-    UI.renderAnswer(currentAnswer, operator);
+  if (firstOperhand && secondOperhand && operator) {
+    const result = performCalculation();
+    answer.innerText = result;
+    firstOperhand = result;
+    secondOperhand = undefined;
+    operator = undefined;
   } else {
-    UI.errorMessage('=')
+    errorMessage('=')
   }
 
   if (isDecimal) {
-    UI.renderMinimizedRemainder();
+    renderMinimizedRemainder();
   };
-
-  UI.restartOperatorsBtnStyle();
+  restartOperatorsBtnStyle();
 })
 
 // Click numbers event listener
 document.querySelectorAll('.number').forEach(number => {
   number.addEventListener('click', function (e) {
-    // Store new number
     const newNumber = e.target.getAttribute('data-value');
-    // Store initial number
-    if (initialNumber === undefined && isInitialNumberOneDigitOrLess) {
-      initialNumber = newNumber
-      process.innerText = initialNumber
-    } else if (isInitialNumberOneDigitOrLess) {
-      initialNumber += newNumber
-      process.innerText = initialNumber
-      // Store last number
-    } else if (lastNumber === undefined && !isInitialNumberOneDigitOrLess) {
-      lastNumber = newNumber
-      process.innerText += lastNumber
+    if (!operator) {
+      setFirstOperhand(newNumber)
     } else {
-      lastNumber += newNumber
-      process.innerText += newNumber
+      setSecondOperhand(newNumber);
     }
-
-    UI.restartOperatorsBtnStyle();
+    restartOperatorsBtnStyle();
   });
 });
+
